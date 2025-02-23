87640eac64a287fcfc85cac9b8a4c9e3b8f01857
iqra <im4356927@gmail.com>
2025-02-23 01:12:03 -0800

    add start script

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+    My  About
+    </div>
+  )
+}
+
+export default About
+if(chatId != newMessageForRealTime.chatId){
+  notificationCount++;
+}
