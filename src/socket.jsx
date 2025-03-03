fd36907023aa22af97881c4600ed902458449ee2
iqra <im4356927@gmail.com>
2025-03-03 14:34:48 -0800

    add url

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,20 +1,66 @@
+// import { createContext, useContext, useEffect, useState } from 'react';
+// import { io } from 'socket.io-client';
+
+// const SocketContext = createContext();
+
+// export const useSocket = () => useContext(SocketContext);
+
+// export const SocketProvider = ({ children }) => {
+//     const [socket, setSocket] = useState(null);
+
+//     useEffect(() => {
+//         const newSocket = io('http://localhost:3000', {
+//             withCredentials: true, // Ensures credentials (cookies, headers) are sent
+//             transports: ['websocket'], // Preferred transport method
+//             extraHeaders: {
+//                 'Access-Control-Allow-Origin': 'http://localhost:5173', // CORS header
+//             },
+//         });
+
+//         // Store the socket connection in state
+//         setSocket(newSocket);
+
+//         // Cleanup on unmount
+//         return () => {
+//             if (newSocket) {
+//                 newSocket.close();
+//             }
+//         };
+//     }, []);
+
+//     return (
+//         <SocketContext.Provider value={socket}>
+//             {children}
+//         </SocketContext.Provider>
+//     );
+// };
+
 
 import { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 const SocketContext = createContext();
-const apiUrl = import.meta.env.VITE_API_URL;
+
 export const useSocket = () => useContext(SocketContext);
 
 export const SocketProvider = ({ children, isAuthenticated }) => {
     const [socket, setSocket] = useState(null);
+
     useEffect(() => {
         if (!isAuthenticated) return; // Wait until user logs in
 
-        const newSocket = io(`https://whisperia-backened-production.up.railway.app`, {
-            withCredentials: true,
-            transports: ["websocket"],
-        });
+        // const newSocket = io("http://localhost:3000", {
+        //     withCredentials: true,
+        //     transports: ["websocket"],
+        // });
+
+        const newSocket = io('https://whisperia-backened-production.up.railway.app', {
+                        withCredentials: true, // Ensures credentials (cookies, headers) are sent
+                        transports: ['websocket'], // Preferred transport method
+                        extraHeaders: {
+                            'Access-Control-Allow-Origin': 'https://whisperia-frontened.vercel.app', // CORS header
+                        }
+                    });
 
         newSocket.on("connect", () => {
             console.log("Connected to WebSocket:", newSocket.id);
